d05a953879a2745f0d0c11d4fa8300c01743d15d
ensarvragic <vragicensar@gmail.com>
2023-08-25 17:06:47 +0200

    inserted logic into search input

--- weather-app/src/components/WeatherApp.jsx
+++ weather-app/src/components/WeatherApp.jsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './WeatherApp.css'
+
 
 import clear_icon from '../assets/clear.png';
 import cloud_icon from '../assets/cloud.png';
@@ -15,6 +16,8 @@ const WeatherApp = () => {
 
     let api_key = `dd94f859a0e52d6e4767fddf735f04a7`;
 
+    const[wicon, setWicon] = useState(cloud_icon);
+
     const search = async () => {
         const element = document.getElementsByClassName("cityInput")
         if(element[0].value === ''){
@@ -22,8 +25,51 @@ const WeatherApp = () => {
         }
         let url = `https://api.openweathermap.org/data/2.5/weather?q=${element[0].value}&units=Metric&appid=${api_key}`
 
-        let respone = await fetch(url);
-        let data = response.json();
+        let response = await fetch(url);
+        let data = await response.json();
+
+        const humidity = document.getElementsByClassName('humidity-percent')
+        const wind = document.getElementsByClassName('wind-rate')
+        const temperature = document.getElementsByClassName('weather-temp')
+        const location = document.getElementsByClassName('weather-location')
+
+        humidity[0].innerHTML = data.main.humidity+'%';
+        wind[0].innerHTML = Math.floor(data.wind.speed)+'km/h';
+        temperature[0].innerHTML = Math.floor(data.main.temp)+'°c';
+        location[0].innerHTML = data.name;
+
+        switch (data.weather[0].icon) {
+            case '01d':
+            case '01n':
+                setWicon(clear_icon);
+                break;
+            case '02d':
+            case '02n':
+                setWicon(cloud_icon);
+                break;
+            case '03d':
+            case '03n':
+                setWicon(drizzle_icon);
+                break;
+            case '04d':
+            case '04n':
+                setWicon(drizzle_icon);
+                break;
+            case '09d':
+            case '09n':
+                setWicon(rain_icon);
+                break;
+            case '10d':
+            case '10n':
+                setWicon(rain_icon);
+                break;
+            case '13d':
+            case '13n':
+                setWicon(snow_icon);
+                break;
+            default:
+                setWicon(clear_icon);
+        }
     }
 
   return (
@@ -50,7 +96,7 @@ const WeatherApp = () => {
             <div className="element">
                 <img src={wind_icon} className='icon'/>
                 <div className="data">
-                    <div className="humidity-percent">18 km/h</div>
+                    <div className="wind-rate">18 km/h</div>
                     <div className="text">Wind Speed</div>
                 </div>
             </div>
